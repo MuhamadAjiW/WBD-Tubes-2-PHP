64b300c00def26a96821111e7234601fe643d306
MuhamadAjiW <16521119@mahasiswa.itb.ac.id>
2023-11-16 17:51:00 +0700

    feat: subscription request feedback

--- src/app/views/Subscriber.php
+++ src/app/views/Subscriber.php
@@ -8,53 +8,55 @@
 </head>
 <?php if(file_exists($TOP_BAR)) include_once($TOP_BAR);?>
 <img class="sty-bckgrnd" src="/storage/assets/logo.svg" alt="Stylized Background">
-<div class="main-content first">
-  <div class="gen-header cluster-h">
-    <h2>Daftar Author Yang Bisa Di Subscribe</h2>
-  </div>
-  <div class="book-grid">
-    <?php
-    if (is_array($data)) {
-      foreach ($data as $key => $value) {
-          if (is_array($value) && isset($value['username'])) {
-              echo '<div class="author-info">';
-              echo '<img src="https://tse4.mm.bing.net/th?id=OIP._arhxJRyb58rGEtxa_v_1QHaHa&pid=Api&P=0&h=180" alt="Author Image">';
-              echo '<h3>' . $value["username"] . '</h3>';
-              
-              if(!isset($value['status'])){
-                echo '<form method="POST" action="/subscribe">';
-                echo '<input type="hidden" name="author_id" value="' . $value["author_id"] . '">';
-                echo '<button class="btn btn-yellow" type="submit" name="subscribe_button">Subscribe</button>';
-                echo '</form>';
-              } else{
-                switch ($value['status']) {
-                  case 'PENDING':
-                    echo 'Request pending';
-                    break;
-                  case 'ACCEPTED':
-                    echo 'Request accepted';
-                    //TODO: add button to go to booklist
-                    break;
-                  case 'REJECTED':
-                    echo 'Request rejected';
-                    //TODO: Do what?
-                    break;
-                  
-                  default:
-                    echo '<form method="POST" action="/subscribe">';
-                    echo '<input type="hidden" name="author_id" value="' . $value["author_id"] . '">';
-                    echo '<button class="btn btn-yellow" type="submit" name="subscribe_button">Subscribe</button>';
-                    echo '</form>';
-                    break;
+<body class="gen-body">
+  <div class="main-content first">
+    <div class="gen-header cluster-h">
+      <h1>Daftar Author Yang Bisa Di Subscribe</h1>
+    </div>
+    <div class="book-grid" style="margin-top: 15px;">
+      <?php
+      if (is_array($data)) {
+        foreach ($data as $key => $value) {
+            if (is_array($value) && isset($value['username'])) {
+                echo '<div class="author-info" style="margin:15px 15px">';
+                echo '<img src="/storage/assets/profile.svg" alt="Author Image">';
+                echo '<h3 class="title-text" style="font-size:16px">' . $value["name"] . '</h3>';
+                
+                if(!isset($value['status'])){
+                  echo '<form method="POST" action="/subscribe">';
+                  echo '<input type="hidden" name="author_id" value="' . $value["author_id"] . '">';
+                  echo '<button class="btn btn-grey" type="submit" name="subscribe_button">Subscribe</button>';
+                  echo '</form>';
+                } else{
+                  switch ($value['status']) {
+                    case 'PENDING':
+                      echo '<p class="subtitle-text" style="font-size:14px">Request pending</p>';
+                      break;
+                    case 'ACCEPTED':
+                      echo '<a class="btn btn-yellow" onclick="location.href=\'/userbooks\'" style="display:inline-block;font-size:13.3333px">Lihat buku</a>';
+                      //TODO: add button to go to booklist
+                      break;
+                    case 'REJECTED':
+                      echo '<p class="subtitle-text" style="font-size:14px;color:red">Request rejected</p>';
+                      //TODO: Do what?
+                      break;
+                    
+                    default:
+                      echo '<form method="POST" action="/subscribe">';
+                      echo '<input type="hidden" name="author_id" value="' . $value["author_id"] . '">';
+                      echo '<button class="btn btn-yellow" type="submit" name="subscribe_button">Subscribe</button>';
+                      echo '</form>';
+                      break;
+                  }
                 }
-              }
-              echo '</div>';
-          }
-      }
-  } else {
-      echo 'Data tidak valid atau kosong.';
-  }
-    ?>
+                echo '</div>';
+            }
+        }
+    } else {
+        echo 'Data tidak valid atau kosong.';
+    }
+      ?>
+    </div>
   </div>
-</div>
+</body>
 </html>
